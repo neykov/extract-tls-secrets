58b2660c7171bd8a4514328d9dce2910f0979d7c
Svetoslav Neykov <svetoslav@neykov.name>
2021-03-06 16:36:34 +0200

    Add test coverage for Java 17 Early Access

--- src/test/docker/test.sh
+++ src/test/docker/test.sh
@@ -17,7 +17,7 @@ CWD="$( cd "$(dirname "$0")" ; pwd -P )"
 ROOT=$( cd "$CWD/../../.." && pwd )
 TEST_TMP="$ROOT/target/test/temp"
 SECRETS_VOLUME="$TEST_TMP/secrets"
-JAVA_VERSIONS="16 15 14 13 12 11 10 9 8 7 6"
+JAVA_VERSIONS="17 16 15 14 13 12 11 10 9 8 7 6"
 
 $CWD/test_errors.sh $JAR_PATH
 
